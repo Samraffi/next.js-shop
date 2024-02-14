e124ce7e827184656f90d0c4561a0e062e9dfbc9
Margaritta2023 <margarittamkn@gmail.com>
2024-02-14 22:26:26 +0400

    feat basket/add styles, delete button

--- src/components/BasketPage/Basket.js
+++ src/components/BasketPage/Basket.js
@@ -1,116 +1,122 @@
-"use client";
+ "use client";
+ import { Box, Button, Checkbox, Container, Grid, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+import Link from 'next/link';
+import { useSelector } from 'react-redux';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-import Link from "next/link";
-import { useSelector } from "react-redux";
-import "./Basket.css";
+const StyledBasket = styled(Container)(({ theme }) => ({
+  padding: theme.spacing(3),
+  marginTop: theme.spacing(3),
+  backgroundColor: theme.palette.background.paper,
+  boxShadow: theme.shadows[5],
+  borderRadius: theme.shape.borderRadius,
+}));
+
+const StyledBasketProductItem = styled(Grid)(({ theme }) => ({
+  marginBottom: theme.spacing(3),
+  borderBottom: `1px solid ${theme.palette.divider}`,
+  paddingBottom: theme.spacing(2),
+  alignItems: 'center',
+}));
+
+const StyledBasketTotal = styled(Box)(({ theme }) => ({
+  marginTop: theme.spacing(3),
+  padding: theme.spacing(2),
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  borderTop: `2px solid ${theme.palette.primary.main}`,
+}));
+
+const StyledProductName = styled(Typography)(({ theme }) => ({
+  fontSize: '1.2rem',
+  fontFamily: "'Quicksand', sans-serif",
+  marginBottom: theme.spacing(1),
+}));
+
+const StyledQuantityContainer = styled(Grid)(({ theme }) => ({
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+}));
+
+const StyledQuantityButtons = styled(Button)(({ theme }) => ({
+  fontWeight: 'bold',
+  fontSize: '1rem',
+  marginRight: theme.spacing(1),
+  borderColor: theme.palette.grey[500],
+  color: theme.palette.grey[500],
+  '&:hover': {
+    backgroundColor: theme.palette.grey[200],
+  },
+}));
+
+const StyledDeleteIcon = styled(DeleteIcon)(({ theme }) => ({
+  color: theme.palette.error.main,
+  '&:hover': {
+    color: theme.palette.error.light,
+  },
+  cursor: 'pointer',
+}));
+
+const StyledBasketBuyButton = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  '&:hover': {
+    backgroundColor: theme.palette.primary.dark,
+  },
+  marginTop: theme.spacing(2),
+}));
 
 function Basket() {
-  const selectBasketItems = useSelector(
-    (state) => state.selectBasketItems.selectBasketItems
-  );
+  const selectBasketItems = useSelector((state) => state.selectBasketItems.selectBasketItems);
 
   return (
-    <div className="basket">
-      <div>
-        <Link href="/">GoTo</Link>
-      </div>
-      <div className="basket-content">
-        <div className="basket-content-left">
-          <div className="basket-product">
-            <h2>Basket</h2>
-            {selectBasketItems.map((item, index) => {
-              return (
-                <div className="basket-product-item" key={index}>
-                  <div className="basket-product-item-img">
-                    <img src={item.images[0]} alt="Image" />
-                  </div>
-                  <div className="basket-product-item-description">
-                    <h2>{item.title}</h2>
-                    <p>${item.price}</p>
-                  </div>
-                  <div className="basket-product-item-quantity">
-                    <div className="quantity-add">-</div>
-                    <div className="quantity-number">1</div>
-                    <div className="quantity-remove">+</div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className="basket-delivery"></div>
-          <div className="basket-PaymentDetails">
-            <div className="basket-payment"></div>
-            <div className="basket-details"></div>
-          </div>
-        </div>
-        {/* <div className="basket-content-right">
-          <span>Product {0} pieces</span>
-          <div className="basket-total">
-            <span>Total</span>
-            <span>0 $</span>
-          </div>
-          <div className="basket-buy">
-            <button className="basket-buy-button">Basket</button>
-          </div>
-          <div className="basket-convention">
-            <span>
-              <input type="checkbox" name="convention" />
-            </span>
-            <span>
-              I agree with the rules for using the trading platform and
-              returning
-            </span>
-          </div>
-        </div> */}
-      </div>
-    </div>
+    <StyledBasket>
+      <Typography variant="h4" align="center" gutterBottom>
+        Basket
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          {selectBasketItems.map((item, index) => (
+            <StyledBasketProductItem key={index} container spacing={2}>
+              <Grid item xs={3}>
+                <img src={item.images[0]} alt="Image" width="100%" style={{ borderRadius: '8px' }} />
+              </Grid>
+              <Grid item xs={6}>
+                <StyledProductName variant="h6">{item.title}</StyledProductName>
+                <Typography>${item.price}</Typography>
+              </Grid>
+              <StyledQuantityContainer item xs={3} container>
+                <StyledQuantityButtons variant="outlined" color="primary">-</StyledQuantityButtons>
+                <Typography variant="body1" fontWeight="bold" fontSize="1.2rem" marginRight="8px">{item.quantity}</Typography>
+                <StyledQuantityButtons variant="outlined" color="primary">+</StyledQuantityButtons>
+                <StyledDeleteIcon />
+              </StyledQuantityContainer>
+            </StyledBasketProductItem>
+          ))}
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Typography variant="h6" align="center" gutterBottom>
+            Product {selectBasketItems.reduce((acc, item) => acc + item.quantity, 0)} pieces
+          </Typography>
+          <StyledBasketTotal>
+            <Typography variant="h6">Total</Typography>
+            <Typography>0 $</Typography>
+          </StyledBasketTotal>
+          <StyledBasketBuyButton variant="contained" fullWidth>
+            Buy Now
+          </StyledBasketBuyButton>
+          <Box mt={2} textAlign="center">
+            <Checkbox name="convention" />
+            <Typography>I agree with the rules for using the trading platform and returning</Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </StyledBasket>
   );
 }
 
 export default Basket;
 
 
-// "use client";
-
-// import { useSelector } from "react-redux";
-// import "./Basket.css";
-
-// function Basket () {
-//   const selectBasketItems = useSelector((state) => state.selectBasketItems.selectBasketItems);
-
-//   const count = selectBasketItems.reduce(
-//     (acc, basketItem) => acc + (basketItem.quantity ? basketItem.quantity : 1),
-//     0
-//   );
-
-//   return (
-//     <div className="basket">
-//       <div className="basket-menu opened">
-//         {selectBasketItems.map((item, index) => {
-//           return (
-//             <div className="basket-menu-items" key={item.id}>
-//               <div className="basket-menu-item">
-//                 <img src={item.images[0]} alt={item.images[0]} />
-//                 <div className="basket-menu-item-description">
-//                   <h2>count: {item.quantity}</h2>
-//                   <h2>{item.title}</h2>
-//                   <p>${item.price}</p>
-//                   <button>Buy</button>
-//                 </div>
-//               </div>
-//             </div>
-//           );
-//         })}
-//       </div>
-
-//       <img
-//         src="/image/basket1.png"
-//         alt="Basket"
-//         className="basket-img"
-//       />
-//       <div className="calc">{count}</div>
-//     </div>
-//   );
-// }
-
-// export default Basket;
