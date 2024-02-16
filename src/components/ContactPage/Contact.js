42e0b480794e714731d5ee3c2baba93c40cb6199
Margaritta2023 <margarittamkn@gmail.com>
2024-02-17 01:12:11 +0400

    add formik, validation,initvalues to contuctUs

--- src/components/ContactPage/Contact.js
+++ src/components/ContactPage/Contact.js
@@ -1,11 +1,31 @@
 "use client";
 
 import "./Contact.css";
+import React from 'react';
+import * as Yup from 'yup';
+import { TextField, Button, Container, Typography } from '@mui/material';
+import { useFormik } from 'formik';
 
 const ContactPage = () => {
-  return (
-    <div>ContactPage</div>
-  );
-}
+  const initialValues = {
+    name: '',
+    surname: '',
+    email: '',
+    text: '',
+  };
+
+  
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Please, enter the name'),
+    surname: Yup.string().required('Please, enter the surname'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    text: Yup.string().required('Please enter text'),
+  });
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
 
 export default ContactPage;
