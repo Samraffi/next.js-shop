deb90218ebcc5f3abb6108658f80ec9970a4e532
Samvel Vardanyan <129245937+DeveloperSamvel@users.noreply.github.com>
2024-02-27 14:01:47 +0400

    refactoring structure in signUp and Login pages

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,94 +1,11 @@
 "use client";
 
-import { Formik, Field, Form } from "formik";
-import * as Yup from "yup";
-import { motion } from "framer-motion";
-import styles from "./login.module.css";
-import Link from "next/link";
-import Header from "@/layouts/Header/Header";
-import { useRouter } from "next/navigation";
-import { useAuthUserAndSignOut } from "@/hooks/useAuthUserAndSignOut";
-import { AuthContext } from "@/context/useAuthContext";
+import Login from "@/components/LoginPage/Login";
 
-const LoginSchema = Yup.object().shape({
-  email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string().required("Required"),
-});
-
-const Login = () => {
-  const { push } = useRouter();
-  const { userSignIn } = useAuthUserAndSignOut();
-
+const LoginPage = () => {
   return (
-    <AuthContext.Provider value={{ userSignIn }}>
-      <Header />
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ delay: 0.5 }}
-      >
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          validationSchema={LoginSchema}
-          onSubmit={(values) => {
-            userSignIn(values)
-              .then((credential) => push('/'))
-              .catch((err) => console.log(err.message));
-          }}
-        >
-          {({ errors, touched }) => (
-            <Form className={`${styles.signForm}`}>
-              <div className={`${styles.container}`}>
-                <label htmlFor="email">
-                  <b>Username</b>
-                </label>
-                <br />
-                <Field
-                  className={`${styles.txtInput}`}
-                  type="text"
-                  name="email"
-                />
-                <div className={`${styles.error}`}>
-                  {errors.email && touched.email ? (
-                    <div>{errors.email}</div>
-                  ) : null}
-                </div>
-                <br />
-                <label htmlFor="password">
-                  <b>Password</b>
-                </label>&nbsp;
-                <br />
-                <Field
-                  className={`${styles.pswInput}`}
-                  type="password"
-                  name="password"
-                />
-                {errors.password && touched.password ? (
-                  <div>{errors.password}</div>
-                ) : null}
-                <motion.button
-                  className={`${styles.btn}`}
-                  type="submit"
-                  whileHover={{ scale: 1.05 }}
-                >
-                  Login
-                </motion.button>
-              </div>
-              <motion.button
-                type="button"
-                className={`${styles.change} ${styles.btn}`}
-                whileHover={{ scale: 1.05 }}
-              >
-                <Link href="/sign-up" className={`${styles.sgn}`}>
-                  Sign Up
-                </Link>
-              </motion.button>
-            </Form>
-          )}
-        </Formik>
-      </motion.div>
-    </AuthContext.Provider>
+    <Login />
   );
-}
+};
 
-export default Login;
+export default LoginPage;
---
+++ src/components/LoginPage/Login.js
@@ -0,0 +1,95 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import * as Yup from "yup";
+import { Formik, Field, Form } from "formik";
+import { motion } from "framer-motion";
+import Header from "@/layouts/Header/Header";
+import { useAuthUserAndSignOut } from "@/hooks/useAuthUserAndSignOut";
+import { AuthContext } from "@/context/useAuthContext";
+
+import styles from "./Login.module.css";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string().required("Required"),
+});
+
+const Login = () => {
+  const { push } = useRouter();
+  const { userSignIn } = useAuthUserAndSignOut();
+
+  return (
+    <AuthContext.Provider value={{ userSignIn }}>
+      <Header />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 0.5 }}
+      >
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={LoginSchema}
+          onSubmit={(values) => {
+            userSignIn(values)
+              .then((credential) => push('/'))
+              .catch((err) => console.log(err.message));
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form className={`${styles.signForm}`}>
+              <div className={`${styles.container}`}>
+                <label htmlFor="email">
+                  <b>Username</b>
+                </label>
+                <br />
+                <Field
+                  className={`${styles.txtInput}`}
+                  type="text"
+                  name="email"
+                />
+                <div className={`${styles.error}`}>
+                  {errors.email && touched.email ? (
+                    <div>{errors.email}</div>
+                  ) : null}
+                </div>
+                <br />
+                <label htmlFor="password">
+                  <b>Password</b>
+                </label>&nbsp;
+                <br />
+                <Field
+                  className={`${styles.pswInput}`}
+                  type="password"
+                  name="password"
+                />
+                {errors.password && touched.password ? (
+                  <div>{errors.password}</div>
+                ) : null}
+                <motion.button
+                  className={`${styles.btn}`}
+                  type="submit"
+                  whileHover={{ scale: 1.05 }}
+                >
+                  Login
+                </motion.button>
+              </div>
+              <motion.button
+                type="button"
+                className={`${styles.change} ${styles.btn}`}
+                whileHover={{ scale: 1.05 }}
+              >
+                <Link href="/sign-up" className={`${styles.sgn}`}>
+                  Sign Up
+                </Link>
+              </motion.button>
+            </Form>
+          )}
+        </Formik>
+      </motion.div>
+    </AuthContext.Provider>
+  );
+}
+
+export default Login;
